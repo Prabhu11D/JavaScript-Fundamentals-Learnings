d0a15634904506f5f4edc421a20374bbd8b5224c
Prabhu <programmerprabhu@gmail.com>
2020-12-01 14:45:20 +0530

    Basic Decorators

---
+++ JavaScript.info/1.Language/6.Advanced_Working_with_Functions/9.Decorators_and_forwarding_call_apply.js
@@ -0,0 +1,95 @@
+/**
+ *      Decorator - Decorators are just a wrapper around a function. 
+ *                  They are used to enhance the functionality of the 
+ *                  function without modifying the underlying function
+ * 
+ */
+
+// ======================   Transparent Caching
+
+function slow(x){
+    return `function at ${x}`
+}
+
+function cacheDecorator(func){
+    let cache = new Map();
+
+    return function(x){
+        if(cache.has(x)){
+            return cache.get(x)
+        }
+        let result = func(x);
+        cache.set(x, result);
+
+        return cache.get(x)
+    }
+}
+
+slow = cacheDecorator(slow);
+
+console.log(slow(1));
+console.log(slow(2));
+console.log(slow(1));
+
+// ======================   Using "func.call" for the context
+
+let worker = {
+  someMethod() {
+    return 1;
+  },
+
+  slow(x) {
+    console.log("Called with " + x);
+    return x * this.someMethod(); // (*)
+  }
+};
+
+function cachingDecoratorFuncCall(func) {
+  let cache = new Map();
+  return function(x) {
+    if (cache.has(x)) {
+      return cache.get(x);
+    }
+    let result = func.call(this, x); // "this" is passed correctly now
+    cache.set(x, result);
+    return result;
+  };
+}
+
+worker.slow = cachingDecoratorFuncCall(worker.slow); // now make it caching
+
+console.log( worker.slow(2) ); // works
+console.log( worker.slow(2) ); // works, doesn't call the original (cached)
+
+
+
+// ======================   Going multi-argument
+
+let workerMultiArg = {
+  slow(min, max) {
+    console.log(`Called with ${min},${max}`);
+    return min + max;
+  }
+};
+function cachingDecoratorMultiArg(func, hash) {
+  let cache = new Map();
+  return function() {
+    let key = hash(arguments);
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+    let result = func.call(this, ...arguments); 
+    cache.set(key, result);
+    return result;
+  };
+}
+function hash(args) {
+  return args[0] + ',' + args[1];
+}
+worker.slow = cachingDecoratorMultiArg(worker.slow, hash);
+console.log( worker.slow(30, 5) );
+console.log( "Again " + worker.slow(30, 5) );
+
+// ======================   func.apply
+// ======================   Borrowing a method
+// ======================   Decorators and function properties
