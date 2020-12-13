7f9e5674ef68a73645ecbd73df47ce6cf45a727a
Prabhu <programmerprabhu@gmail.com>
2020-12-13 16:13:33 +0530

    Singleton pattern

---
+++ Design Patterns/DevSage/Singleton.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ *      it is creational pattern
+ *     it only creates one object
+ * 
+ */
+
+const Singleton = (function(){
+  let pManager
+
+  function ProcessManager() { /*...*/ }
+
+  function createProcessManager()
+  {
+    pManager = new ProcessManager()
+    return pManager
+  }
+
+  return {
+      getProcessManager: () =>
+      {
+        if(!pManager)
+          pManager = createProcessManager()
+        return pManager
+      }
+  }
+})();
+
+const singleton = Singleton.getProcessManager()
+const singleton2 = Singleton.getProcessManager()
+
+console.log(singleton === singleton2)
