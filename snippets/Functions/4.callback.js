0cc6437b5c720c7ce933718e9ea0d337f65f0db7
Prabhu <programmerprabhu@gmail.com>
2020-12-15 16:19:53 +0530

    Callback hell

--- snippets/Functions/4.callback.js
+++ snippets/Functions/4.callback.js
@@ -9,3 +9,17 @@ console.log(calc(add,10,10));
 console.log(calc(sub,10,10));
 console.log(calc(mul,10,10));
 console.log(calc(div,10,10));
+
+function first(callback1, callback2){
+    console.log("Hey I'm First Function");
+    callback1(callback2);
+}
+function second(callback1){
+    callback1();
+    console.log("Hey I'm Second Function");
+}
+function third(){
+    console.log("Hey I'm Third Function");
+}
+
+first(second, third);
