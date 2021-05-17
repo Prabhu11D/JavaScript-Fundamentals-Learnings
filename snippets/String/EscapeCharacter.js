a33a5cfcf4a1a6b5257fb71027cae02eda8641dd
Prabhu K <52393935+programmerprabhu@users.noreply.github.com>
2021-05-17 17:48:20 +0530

    String Escape Characters

---
+++ snippets/String/EscapeCharacter.js
@@ -0,0 +1,9 @@
+console.log("'");
+console.log('"');
+console.log("\\");
+console.log("Hello\nWorld");
+console.log("Hello\tWorld");
+console.log("\t\t\rHello World");
+console.log("Hello\vWorld");
+console.log("Hello\b\b\bWorld");
+console.log("Hello\fWorld");
