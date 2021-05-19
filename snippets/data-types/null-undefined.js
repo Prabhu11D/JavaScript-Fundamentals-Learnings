b6bab1bb16dabee701835bbd971b07a7f30174c9
Prabhu K <52393935+programmerprabhu@users.noreply.github.com>
2021-05-19 15:20:10 +0530

    null vs undefined

---
+++ snippets/data-types/null-undefined.js
@@ -0,0 +1,28 @@
+var name;
+console.log(name);
+var age = null;
+console.log(age);
+
+// type checking
+console.log("\n------- Typeof ---------");
+console.log(typeof null);
+console.log(typeof undefined);
+
+console.log("\n------- Equality checking ---------");
+console.log(null == undefined);
+console.log(null === undefined);
+
+console.log("\n------- null operations -----------");
+console.log(null);
+console.log(!null);
+console.log(!!null);
+console.log(+null);
+console.log(1 + null);
+console.log(null === null);
+console.log(null == null);
+
+console.log("\n-------- undefined ----------");
+console.log(undefined);
+console.log(+undefined);
+console.log(undefined == undefined);
+console.log(undefined === undefined);
